e763f689418cd11e06b70fc0010548822bafea1e
ssb-ae-git <arerasani@gmail.com>
2026-04-13 19:32:33 -0400

    Modified the application code - 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This is next version of my code - Jenkins + Docker CI/CD Lab!!"
+    return "This is next version of my code - Jenkins + Docker CI/CD Lab!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
